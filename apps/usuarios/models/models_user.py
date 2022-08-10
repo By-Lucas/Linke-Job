946cd7a39e6a5047194dead8d81263033ca8ed21
By-Lucas <lukasmulekezika2@gmail.com>
2022-08-10 11:25:57 -0300

    Create views and moodels(profile) of user

---
+++ apps/usuarios/models/models_user.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+
+
+def upload_to(instance ,filename):
+    return 'user/{username}/{filename}'.format(
+        username=instance.user.username, filename=filename)
+
+class ProfileUser(models.Model):
+    TIPO_USUARIO = (
+        ('U', 'Usuario'),
+        ('E', 'Produtor'),
+        )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_tipo = models.CharField(choices=TIPO_USUARIO, max_length=20, null=True, blank=True, db_column='user_tipo', default='U')
+    idade = models.IntegerField(null=True, blank=True, default=0, db_column='idade')
+    cidade = models.CharField(max_length=20, null=True, blank=True, db_column='cidade')
+    contato = models.CharField(max_length=11, null=True, blank=True, db_column='contato')
+    endereco = models.CharField(max_length=20, null=True, blank=True, db_column='endereco')
+    numero_casa = models.IntegerField(null=True, blank=True, db_column='numero_casa')
+    cpf = models.CharField(max_length=14, null=True, blank=True, db_column='cpf')
+    cnpj = models.CharField(max_length=18, null=True, blank=True, db_column='cnpj')
+    endereco = models.CharField(max_length=60, null=True, blank=True, db_column='endereco')
+    sobre = models.TextField(max_length=500, null=True, blank=True, db_column='sobre')
+    facebook_user = models.CharField(max_length=100, null=True, blank=True)
+    instagram_user = models.CharField(max_length=100, null=True, blank=True)
+    linkedin_user = models.CharField(max_length=100, null=True, blank=True)
+    imagem_perfil = models.ImageField(default='usuario.png', upload_to=upload_to, null=True, blank=True, db_column='imagem_perfil')
+    data_cadastro = models.DateField(null=True, blank=True, auto_created=True, auto_now=True, db_column='data_cadastro')
+    data_update = models.DateTimeField(auto_now=True, db_column='data_update')
+
+
+    def get_user_tipo(self):
+        if self.user_tipo == 'C':
+            tipo_user = 'Usuario'
+        if self.user_tipo == 'E':
+            tipo_user = 'Empresa'
+        return tipo_user
+
+    class Meta:
+        db_table = 'profile_user'
+        verbose_name = 'ProfileUser'
+        verbose_name_plural = 'ProfileUsers'
+    
+    def __str__(self):
+        return self.user.username 
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        ProfileUser.objects.create(user=instance)
