eaff77892df4169efaeb55745dce8cfd14638545
By-Lucas <lukasmulekezika2@gmail.com>
2022-08-10 12:06:59 -0300

    update views user and create views auth

--- apps/usuarios/views/views_user.py
+++ apps/usuarios/views/views_user.py
@@ -1,15 +1,44 @@
-from django.shortcuts import redirect, render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import Group, User
-from django.contrib.auth import views as auth_views
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 
 from django.contrib import messages
 from django.contrib.messages import constants
 
-from forms.forms_user import UserForm, ProfileUserForm
+from forms.forms_user import UserForm, ProfileUserForm, SignUpForm
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('index')
+    template_name = 'auth/sign-up.html'
+
+    def form_valid(self, form):
+        fornecedor_cliente = self.request.POST.get('fornecedor_cliente')
+        if fornecedor_cliente == 'cliente' or fornecedor_cliente == '':
+            grupo = get_object_or_404(Group, name='usuario')
+        else:
+            grupo = get_object_or_404(Group, name='empresa')
+
+        url = super().form_valid(form)
+        self.object.groups.add(grupo)
+        self.object.save()
+
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        print('Senha:',password)
+        logando = authenticate(self.request, username=username, password=password)
+        login(self.request, logando)
+        messages.add_message(self.request, constants.SUCCESS, f'Usuário {username} cadastrado e logado com sucesso!')
+        return url
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = 'Cadastre-se'
+        context['botao'] = 'Cadastrar'
+        return context
 
 
 class ProfileUpdateView(TemplateView):
